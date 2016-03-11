8df229b2d13f89568594e4273121b8eccb7e39ae
Marius Hintsche <marius.hintsche@uni-potsdam.de>
2016-03-11 14:13:02 +0100

    add custom vim spell files

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc inputrc Xresources tmux.conf gitconfig gitignore_global bash_aliases"    # list of files/folders to symlink in homedir
+files="vimrc inputrc Xresources tmux.conf gitconfig gitignore_global bash_aliases vim/spell/en.utf-8.add vim/spell/de.utf-8.add"
 #files="bashrc vimrc vim zshrc oh-my-zsh private scrotwm.conf Xresources"
 
 ##########
